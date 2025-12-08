409e66d9786002081a5d99d1609ef8388621d492
Uriel Cabello Sosa <software-ti@ldrsolutions.com.mx>
2025-12-08 16:25:08 -0600

    cambio uriel

--- Views/Inv_inventario/inv_inventario.php
+++ Views/Inv_inventario/inv_inventario.php
@@ -18,7 +18,7 @@
 
                                 <div class="page-title-right">
                                     <ol class="breadcrumb m-0">
-                                        <li class="breadcrumb-item"><a href="javascript: void(0);">MRP</a></li>
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">MRP - tezt</a></li>
                                         <li class="breadcrumb-item active"><?= $data['page_tag'] ?></li>
                                     </ol>
                                 </div>
