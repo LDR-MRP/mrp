5ef24bf682a9aad5a179178a65c9f8fcb7432b01
Erick Pulido <erick.pulido@ldrsolutions.com.mx>
2026-05-27 02:01:05 -0600

    fix: implementar cache busting en scripts para evitar problemas de cache

--- Views/Template/footer_admin.php
+++ Views/Template/footer_admin.php
@@ -862,7 +862,7 @@ Se utiliza con fines de configuración y ajustes visuales del layout.
     <!-- Sweet alert init js-->
     <!-- <script src="<?= media(); ?>/minimal/js/pages/sweetalerts.init.js"></script> -->
 
-    <script src="<?= media(); ?>/js/sys_core.js?v=1.0.4"></script>
+    <script src="<?= media(); ?>/js/sys_core.js?v=1.0.5"></script>
     <script src="<?= media(); ?>/js/modulos/<?= $data['page_functions_js']; ?>?v=1.0.0.5"></script>
 
 
